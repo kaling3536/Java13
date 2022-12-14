fecf3f1eb98b0b844c06387528d59ca25e3a7765
kaling3536 <107583599+kaling3536@users.noreply.github.com>
2022-12-14 12:42:34 +0800

    Create Class02.java

---
+++ Class02.java
@@ -0,0 +1,14 @@
+public class hw13_2 {
+	public static void main(String args[]) {
+		int num = 12, den = 0;
+		int ans;
+
+		if (den == 0)
+			System.out.println("除數為 0");
+		else {
+			ans = num / den;
+			System.out.println("ans=" + ans);
+		}
+		System.out.println("end of main() method!!");
+	}
+}
